51c33d94854e7e2c51cb9ca285a48641872ee533
Lucas Chittka <l.chittka@parkefficient.de>
2026-04-16 17:47:05 +0200

    install gsap

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,11 @@
 <template>
     <div class="h-screen content-center bg-white text-3xl">
-        <div class="h-100 w-100 rounded-full mx-auto bg-teal-900 content-center text-center"><span class="text-teal-300">PARK</span><span class="text-white">EFFICIENT</span></div>
+        <div ref="logo" class="h-100  w-100 rounded-full mx-auto bg-teal-900 content-center text-center"><span
+            class="text-teal-300">PARK</span><span class="text-white">EFFICIENT</span></div>
     </div>
 </template>
 <script setup>
+
+
+
 </script>
