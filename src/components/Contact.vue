52dccb4ba538e7560cd9e0083612651976f04777
Lucas Chittka <l.chittka@parkefficient.de>
2026-04-16 21:16:54 +0200

    contact

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,5 +1,26 @@
 <template>
-    <div class="bg-slate-100 h-screen content-center text-center">Contact</div>
+    <div class="bg-slate-100 h-screen content-center text-center">
+        <button v-if="!showMessage" @click="next" @mouseenter="expandButton" @mouseleave="reduceButton" ref="contact"
+                class="hover:bg-teal-950 cursor-pointer inline-block h-[80px] w-[220px] rounded-full bg-teal-900 text-white">
+            <p>Kontakt</p>
+        </button>
+        <p class="text-xl text-teal-900" v-if="showMessage">$$$ Jackpot, Sie dürfen mit Kai sprechen!</p>
+    </div>
 </template>
 <script setup>
+import {ref} from 'vue';
+
+
+const showMessage = ref(false);
+
+
+
+const next = () => {
+    setTimeout(()=>{
+        showMessage.value = true;
+    }, 300)
+
+}
+
+
 </script>
