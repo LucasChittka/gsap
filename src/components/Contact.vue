c8dacf4e0efe5704dbc1109282ddfe3e269b06ae
Lucas Chittka <l.chittka@parkefficient.de>
2026-04-16 21:15:46 +0200

    more-animation

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,5 +1,48 @@
 <template>
-    <div class="bg-slate-100 h-screen content-center text-center">Contact</div>
+    <div class="bg-slate-100 h-screen content-center text-center">
+        <button v-if="!showMessage" @click="next" @mouseenter="expandButton" @mouseleave="reduceButton" ref="contact"
+                class="hover:bg-teal-950 cursor-pointer inline-block h-[80px] w-[220px] rounded-full bg-teal-900 text-white">
+            <p>Kontakt</p>
+        </button>
+        <p class="text-xl text-teal-900" v-if="showMessage">$$$ Jackpot, Sie dürfen mit Kai sprechen!</p>
+    </div>
 </template>
 <script setup>
+import {gsap} from 'gsap';
+import {ref} from 'vue';
+
+const contact = ref();
+
+const showMessage = ref(false);
+
+const expandButton = () => {
+    gsap.to(contact.value, {
+        height: '220px',
+        duration: 0.2,
+        ease: 'easeInOut'
+    })
+}
+
+const reduceButton = () => {
+    gsap.to(contact.value, {
+        height: '80px',
+        duration: 0.2,
+        ease: 'easeInOut'
+    })
+}
+
+const next = () => {
+    gsap.to(contact.value, {
+        scale: 0.4,
+        opacity: 0,
+        duration: 0.3,
+        ease: 'easeInOut'
+    })
+    setTimeout(()=>{
+        showMessage.value = true;
+    }, 300)
+
+}
+
+
 </script>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,9 @@
 <template>
     <header class="z-50 fixed top-0 inset-x-0 flex justify-center">
         <nav class="flex justify-center space-x-10 px-10 py-2 rounded-full mt-6 bg-slate-200/60 backdrop-blur">
-            <a href="#home" :class="{ 'font-semibold': activeSection === 'home' }">Home</a>
-            <a href="#details" :class="{ 'font-semibold': activeSection === 'details' }">Details</a>
-            <a href="#contact" :class="{ 'font-semibold': activeSection === 'contact' }">Contact</a>
+            <a href="#home" class="font-light" :class="{ 'font-semibold': activeSection === 'home' }">Home</a>
+            <a href="#details" class="font-light" :class="{ 'font-semibold': activeSection === 'details' }">Details</a>
+            <a href="#contact" class="font-light"  :class="{ 'font-semibold': activeSection === 'contact' }">Contact</a>
 
         </nav>
     </header>
