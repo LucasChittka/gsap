2d868878588dfa4e5cb6199e645db357ab35935b
Lucas Chittka <l.chittka@parkefficient.de>
2026-04-17 09:11:43 +0200

    contact-upgrade

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,24 +1,31 @@
 <template>
-    <div class="bg-slate-100 h-screen content-center text-center">
-        <button ref="contact" v-if="!showMessage" @click="next"
+    <div class="bg-slate-100 h-screen content-center text-center relative">
+        <button v-if="!showMessage" @click="next" @mouseenter="expandButton" @mouseleave="reduceButton" ref="contact"
                 class="hover:bg-teal-950 cursor-pointer inline-block h-[80px] w-[220px] rounded-full bg-teal-900 text-white">
             <p>Kontakt</p>
         </button>
-        <p class="text-xl text-teal-900" v-if="showMessage">$$$ Jackpot, Sie dürfen mit Kai sprechen!</p>
+        <div v-if="showMessage" class="absolute inset-0 flex justify-center items-center">
+            <div class="border border-teal-200 rounded-full shadow bg-teal-800 text-xl text-white w-[400px] h-[400px] text-center content-center">Krasses Formular oder so</div>
+
+        </div>
     </div>
 </template>
 <script setup>
+import {gsap} from 'gsap';
 import {ref} from 'vue';
 
+const contact = ref();
 
 const showMessage = ref(false);
+const blockMouseLeave = ref(false);
+
 
 
 
 const next = () => {
     setTimeout(()=>{
         showMessage.value = true;
-    }, 300)
+    }, 500)
 
 }
 
